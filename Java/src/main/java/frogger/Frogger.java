2426a240c00f3b5f2f4a47d53e34a1a511836e57
marcuswh <marcuswh@andrew.cmu.edu>
2026-02-13 12:38:17 -0500

    Task 2 Complete

--- Java/src/main/java/frogger/Frogger.java
+++ Java/src/main/java/frogger/Frogger.java
@@ -13,19 +13,14 @@ public class Frogger {
     
     // Field for task 2. Anything to add/change?
     private final Records records;
-    private String firstName, lastName, phoneNumber, zipCode, state, gender;
+    private final FroggerID froggerId;
 
     public Frogger(Road road, int position, Records records, String firstName, String lastName, String phoneNumber,
     String zipCode, String state, String gender) {
         this.road = road;
         this.position = position;
         this.records = records;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.zipCode = zipCode;
-        this.state = state;
-        this.gender = gender;
+        this.froggerId = new FroggerID(firstName, lastName, phoneNumber, zipCode, state, gender);
     }
 
     /**
@@ -57,8 +52,8 @@ public class Frogger {
      * @return true if record successful, else false.
      */
     public boolean recordMyself() {
-      boolean success = records.addRecord(firstName, lastName, phoneNumber, zipCode, state, gender);
-      return success;
+            boolean success = records.addRecord(froggerId);
+            return success;
     }
 
 }
